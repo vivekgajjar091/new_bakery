af6cee9db1da2406fadc9c83b1aa7a01fe3c5f51
vivek-panchasara <panchasravivek1@gmail.com>
2026-03-26 14:48:09 +0530

    Connected MySQL database

--- bakehouse/settings.py
+++ bakehouse/settings.py
@@ -68,6 +68,16 @@ STORAGES = {
 
 
 # Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
